af714b230267b260b458ccb71303c550b7a473e0
omer.ceder <omerceder@gmail.com>
2021-11-15 09:57:41 +0200

    Rest API attempt

--- wp-content/themes/twentytwenty-child/inc/helpers.php
+++ wp-content/themes/twentytwenty-child/inc/helpers.php
@@ -62,3 +62,38 @@ function product_box( $product_id, $box_color='', $is_shortcode = false ) {
 function print_product_box( $product_id, $box_color='', $is_shortcode = false ) {
   echo product_box( $product_id, $box_color='', $is_shortcode = false );
 }
+
+
+// Rest API Endpoint
+
+add_action('rest_api_init', function () {
+  register_rest_route( 'twentytwentyone-child/v1', 'latest-products/(?P<product_cat>\d+)',array(
+                'methods'  => 'GET',
+                'callback' => 'get_products_by_category'
+      ));
+});
+
+function get_products_by_category($request) {
+
+    $args = array (
+        'post_type' => 'product',
+        'tax_query'     => array(
+            array(
+                'taxonomy' => 'product_cat',
+                'field'    => 'term_id',
+                'terms'    => $request['product_cat'],
+            ),
+        ),
+    );
+
+    $posts = get_posts($args);
+
+    if (empty($posts)) {
+      return new WP_Error( 'empty_category', 'There are no posts to display', array('status' => 404) );
+    }
+
+    $response = new WP_REST_Response($posts);
+    $response->set_status(200);
+
+    return $response;
+}
