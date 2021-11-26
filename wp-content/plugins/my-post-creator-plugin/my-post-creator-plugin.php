e6e7da3e3cf006deae75682996ce4b74cc21525b
omer.ceder <omerceder@gmail.com>
2021-11-26 10:30:52 +0200

    Plugin folder & main file

---
+++ wp-content/plugins/my-post-creator-plugin/my-post-creator-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: My Post Creator Plugin
+ * Plugin URI: ''
+ * Description: A plugin to create posts
+ * Version: 1.0
+ * Author: Omer Ceder
+ * Author URI: https://github.com/omerceder/
+ */
